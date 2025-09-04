4dd8d4d7e480e22873fc6bdddfdb17d0a78fa848
Wes Reed <108896513+reedws@users.noreply.github.com>
2025-09-04 12:12:56 -0400

    Update organizations.ts

--- src/app/config/organizations.ts
+++ src/app/config/organizations.ts
@@ -1,22 +1,12 @@
 // Organization and assignment mappings
 export const ORGANIZATIONS = {
-  "IT3049C-Fall25": {
-    name: "IT3049C Fall25",
-    owner: "IT3049C", // GitHub organization/owner name
+  "IT3038C-Reed-FA25": {
+    name: "IT3038C FA25",
+    owner: "IT3038C-Reed-FA25", // GitHub organization/owner name
     assignments: {
-
-      "🧑‍💻Lab: Git Started - Your Profile": "git-started-with-you-profile",
-      "🧑‍💻 Campus Portal (JS Exercises)": "js-exercise-campus-portal",
+      "Bash Basics": "bash-basics",
     }
   },
-  "IT2053C-Fall25": {
-    name: "IT2053C Fall25",
-    owner: "IT2053C-Gilany", // GitHub organization/owner name
-    assignments: {
-      "Env Setup Check": "env-setup-check",
-      "Python Bootcamp": "python-bootcamp",
-    }
-  }
 } as const;
 
 export type OrganizationKey = keyof typeof ORGANIZATIONS;
