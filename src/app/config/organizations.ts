65e965edf27c399eb48824a44e4f796b7b18cc0f
Wes Reed <reedws@ucmail.uc.edu>
2025-09-04 12:40:53 -0400

    added rest of assignments

--- src/app/config/organizations.ts
+++ src/app/config/organizations.ts
@@ -5,6 +5,17 @@ export const ORGANIZATIONS = {
     owner: "IT3038C-Reed-FA25", // GitHub organization/owner name
     assignments: {
       "Bash Basics": "bash-basics",
+      "Bash Arguments": "bash-arguments",
+      "Bash Files": "bash-files",
+      "Bash Project": "bash-project",
+      "Bash RegEx": "bash-regex",
+      "Python Args": "python-args",
+      "Python Basics": "python-basics",
+      "Python CSVs": "python-csvs",
+      "Python Files": "python-files",
+      "Python Project": "python-project",
+      "Python RegEx": "python-regex",
+      "Python SQL": "python-sql"
     }
   },
 } as const;
